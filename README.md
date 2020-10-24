1a12f8a089cbac93aabe76f02a862999ee7e2193
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-10-24 16:08:14 +0300

    add canIterate function to app.js

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/2mdft4xod90ni98o?svg=true)](https://ci.appveyor.com/project/qvvverty/ajs-symbols-iterators-generators-3)
-# ajs symbols, iterators, generators task 1
+# ajs symbols, iterators, generators task 3
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,15 @@
-import Team from './team/team';
-
-const team = new Team();
-for (const member of team) {
-  console.log(member);
+function canIterate(object) {
+  try {
+    return typeof object[Symbol.iterator] === 'function';
+  } catch (e) {
+    return false;
+  }
 }
+
+console.log(canIterate(new Map())); // true
+console.log(canIterate(new Set())); // true
+console.log(canIterate(null)); // false
+console.log(canIterate(undefined)); // false
+console.log(canIterate(NaN)); // false
+console.log(canIterate(10)); // false
+console.log(canIterate('Netology')); // true
